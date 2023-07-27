c16812b7081152f9ff286d1356e368288ac2557b
Nguyen Huy Cuong <nhclnhc@gmail.com>
2023-07-27 23:41:49 +0700

    starter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import Button from '@/components/Button/buton';
 export default function Home() {
-    return (
-        <main className={styles.main}>
-            <Button>clone</Button>
-        </main>
-    );
+    return <main className={styles.main}></main>;
 }
